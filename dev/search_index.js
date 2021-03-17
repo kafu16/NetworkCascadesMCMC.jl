66515a1e304be8c5cdf1f67231a85cc79a5e78d7
Documenter.jl <documenter@juliadocs.github.io>
2021-03-17 18:21:36 +0000

    build based on 12b2de5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NetworkCascadesMCMC","category":"page"},{"location":"#NetworkCascadesMCMC","page":"Home","title":"NetworkCascadesMCMC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NetworkCascadesMCMC]","category":"page"}]
+}
