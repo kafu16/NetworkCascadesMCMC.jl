d210396ae1aa16d63bd0862765d4823aaf3edda1
kafu16 <24458047+kafu16@users.noreply.github.com>
2021-03-17 20:53:38 +0100

    changed template

--- src/NetworkCascadesMCMC.jl
+++ src/NetworkCascadesMCMC.jl
@@ -1,5 +1,6 @@
 module NetworkCascadesMCMC
 
 # Write your package code here.
+# dont export funcs that aren't made for interactive use and are used only locally in a script
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "NetworkCascadesMCMC.jl" begin
     # Write your tests here.
+
+    #@test my_testfunc() == xy
 end
