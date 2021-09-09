8a9a2995dc2c4092b00cc7c635682ed914a85705
kafu16 <24458047+kafu16@users.noreply.github.com>
2021-09-09 16:03:39 +0200

    simplification gen_square_grid

--- src/network_topologies.jl
+++ src/network_topologies.jl
@@ -17,30 +17,51 @@ using LightGraphs, GraphPlot
 # end
 
 
-### generation of non-embedded square grid
-function gen_square_grid(N_side) # N_side: this number sqared gives number of vertices, for N_side > 2
-    N_vertices = N_side * N_side # N_vertices: number of vertices
+function gen_square_grid(N_side)
+    N_vertices = N_side * N_side
 
     g = LightGraphs.SimpleGraph(N_vertices) # generates undirected graph
 
-    # building of square grid
-    # loop one: going through each row of square grid (except last vertex of row, except last row) connecting
-    # each vertex with the vertex of its right and with the vertex under it
-    for i in 1:N_side:N_vertices - 2 * N_side +1
+    # loop one: going through each row of square grid connecting each vertex with the vertex of its right
+    for i in 1:N_side:N_vertices - N_side + 1
         for i in i:i + N_side - 2
-            LightGraphs.add_edge!(g, i, i+1) # LightGraphs. because of "both Graphs and EmbeddedGraphs export "add_edge!"; uses of it in module Main must be qualified"
-            LightGraphs.add_edge!(g, i, i+N_side)
+            LightGraphs.add_edge!(g, i, i+1)
         end
     end
 
-    # loop two: going through last column of square grid connecting each vertex with the vertex under it
-    for i in N_side:N_side:N_vertices - N_side
-        LightGraphs.add_edge!(g, i, i + N_side)
-    end
-
-    # loop three: going through last row of square grid connecting each vertex with the vertex of its right
-    for i in N_vertices - N_side + 1:N_vertices -1
-        LightGraphs.add_edge!(g, i, i+1)
+    # loop two: going through each column of square grid connecting each vertex with the vertex under it
+    for i in 1:N_side
+        for i in i:N_side:N_vertices - (2 * N_side) + i
+            LightGraphs.add_edge!(g, i, i + N_side)
+        end
     end
     g
 end
+
+# ### generation of non-embedded square grid (deprecated function)
+# function gen_square_grid(N_side) # N_side: this number sqared gives number of vertices, for N_side > 2
+#     N_vertices = N_side * N_side # N_vertices: number of vertices
+#
+#     g = LightGraphs.SimpleGraph(N_vertices) # generates undirected graph
+#
+#     # building of square grid
+#     # loop one: going through each row of square grid (except last vertex of row, except last row) connecting
+#     # each vertex with the vertex of its right and with the vertex under it
+#     for i in 1:N_side:N_vertices - 2 * N_side +1
+#         for i in i:i + N_side - 2
+#             LightGraphs.add_edge!(g, i, i+1) # LightGraphs. because of "both Graphs and EmbeddedGraphs export "add_edge!"; uses of it in module Main must be qualified"
+#             LightGraphs.add_edge!(g, i, i+N_side)
+#         end
+#     end
+#
+#     # loop two: going through last column of square grid connecting each vertex with the vertex under it
+#     for i in N_side:N_side:N_vertices - N_side
+#         LightGraphs.add_edge!(g, i, i + N_side)
+#     end
+#
+#     # loop three: going through last row of square grid connecting each vertex with the vertex of its right
+#     for i in N_vertices - N_side + 1:N_vertices -1
+#         LightGraphs.add_edge!(g, i, i+1)
+#     end
+#     g
+# end
