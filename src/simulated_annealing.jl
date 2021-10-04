6b4d4cd4935d10786e4a9982c377b802556a7b42
kafu16 <24458047+kafu16@users.noreply.github.com>
2021-10-04 18:38:48 +0200

    ...

--- src/simulated_annealing.jl
+++ src/simulated_annealing.jl
@@ -238,14 +238,18 @@ function temp_ex2_a(k::Integer, steps_per_temp::Integer)
 end
 
 function temp_ex3_a(k::Integer, steps_per_temp::Integer)
-    5. * 0.99 ^ (floor(k/steps_per_temp)) # floor(x) returns the nearest integral value of the same type as x that is less than or equal to x
-end
-
-function temp_ex3_b(k::Integer, steps_per_temp::Integer)
     2. * 0.99 ^ (floor(k/steps_per_temp)) # floor(x) returns the nearest integral value of the same type as x that is less than or equal to x
 end
 
-function temp_ex3_c(k::Integer, steps_per_temp::Integer)
+function temp_ex3_b(k::Integer, steps_per_temp::Integer)
+    2. * 0.99 ^ (floor(k/steps_per_temp)) + 0.25 # floor(x) returns the nearest integral value of the same type as x that is less than or equal to x
+end
+
+function temp_ex4_a(k::Integer, steps_per_temp::Integer)
+    5. * 0.99 ^ (floor(k/steps_per_temp)) # floor(x) returns the nearest integral value of the same type as x that is less than or equal to x
+end
+
+function temp_ex5_a(k::Integer, steps_per_temp::Integer)
     10. * 0.99 ^ (floor(k/steps_per_temp)) # floor(x) returns the nearest integral value of the same type as x that is less than or equal to x
 end
 
